62f2e22aa1c185af8696411bb39921a74b024c17
cdshenlong1 <cdshenlong1@jd.com>
2020-02-10 20:07:15 +0800

    [init datasource]

--- garbage-baby-web/src/main/java/com/gb/garbage/garbagebaby/service/EchoServiceImpl.java
+++ garbage-baby-web/src/main/java/com/gb/garbage/garbagebaby/service/EchoServiceImpl.java
@@ -6,7 +6,7 @@ import org.apache.dubbo.config.annotation.Service;
  * @author shenlong
  * @date 2020-02-10
  */
-@Service(version = "1.0.0")
+@Service(version = "1.0.1")
 public class EchoServiceImpl implements EchoService {
     @Override
     public void sayHello() {
